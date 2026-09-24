f5db0af1194230f5b8d43c8164994c9226d79ce9
Denys <denny@Denyss-MacBook-Pro.local>
2026-09-24 18:36:32 +0200

    fix all bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,21 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+
 const htmlWidth = document.documentElement.clientWidth;
 const htmlHeight = document.documentElement.clientHeight;
 
-const spiderCenter = htmlWidth / 2;
-const spiderHeight = htmlHeight / 2;
 
-wall.style.width = `${htmlWidth}px`;
-wall.style.height = `${htmlHeight}px`;
-wall.style.position = 'relative';
+wall.style.position = 'relative'
 
-spider.style.position = 'absolute';
-spider.style.width = '50px';
-spider.style.display = 'inline-block';
-spider.style.left = `${spiderCenter}px`;
-spider.style.top = `${spiderHeight}px`;
+spider.style.position = 'absolute'
+
+const spiderWidthSet = spider.offsetWidth;
+const spiderHeightSet = spider.offsetHeight;
+
+const topPosition = (htmlHeight - spiderWidthSet) / 2;
+const leftPosition = (htmlWidth - spiderHeightSet) / 2;
+
+
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
