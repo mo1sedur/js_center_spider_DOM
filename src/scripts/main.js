95b1e9be4daedf322e2e37e9b0bc198fafee5305
Denys <denny@Denyss-MacBook-Pro.local>
2026-09-24 22:38:34 +0200

    i hope so that its the end

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,19 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const htmlWidth = document.documentElement.clientWidth;
-const htmlHeight = document.documentElement.clientHeight;
+// spider.style.width = '30px';
+
+const htmlWidth = wall.offsetWidth;
+const htmlHeight = wall.offsetHeight;
 
 
 wall.style.position = 'relative'
 
-spider.style.position = 'absolute'
+spider.style.position = 'absolute';
 
 const spiderWidthSet = spider.offsetWidth;
 const spiderHeightSet = spider.offsetHeight;
+
 
 const topPosition = (htmlHeight - spiderHeightSet) / 2;
 const leftPosition = (htmlWidth - spiderWidthSet) / 2;
@@ -20,3 +23,4 @@ const leftPosition = (htmlWidth - spiderWidthSet) / 2;
 
 spider.style.top = `${topPosition}px`;
 spider.style.left = `${leftPosition}px`;
+
