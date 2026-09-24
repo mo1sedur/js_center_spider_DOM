6653868169e97cea0b6120eeb69a820421bc106f
Denys <denny@Denyss-MacBook-Pro.local>
2026-09-24 15:45:43 +0200

    hope so that this is right solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const htmlWidth = document.documentElement.clientWidth;
+const htmlHeight = document.documentElement.clientHeight;
+
+const spiderCenter = htmlWidth / 2;
+const spiderHeight = htmlHeight / 2;
+
+wall.style.width = `${htmlWidth}px`;
+wall.style.height = `${htmlHeight}px`;
+wall.style.position = 'relative';
+
+spider.style.position = 'absolute';
+spider.style.width = '50px';
+spider.style.display = 'inline-block';
+spider.style.left = `${spiderCenter}px`;
+spider.style.top = `${spiderHeight}px`;
